84dda34d9f27d119baf470faf53724df1316aaa1
xpanvictor <xpanvictor@gmail.com>
2023-03-17 22:01:55 +0100

    refr: uses struct implementation to make config object

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {} in {}", config.query, config.filename);
 
@@ -18,8 +18,11 @@ struct Config {
     filename: String
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
