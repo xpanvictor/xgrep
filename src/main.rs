78aff27812c8500d0505ba4c519900a9717998b4
xpanvictor <xpanvictor@gmail.com>
2023-03-18 00:27:33 +0100

    refr: moves functionality to lib crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, fs, process, error::Error};
+use std::{env, process};
+
+use xgrep::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,27 +18,3 @@ fn main() {
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments; first is search query, second is filepath");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
