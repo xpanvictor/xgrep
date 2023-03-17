283ab57161745f3cee14373bba09331a5ecacd39
xpanvictor <xpanvictor@gmail.com>
2023-03-17 19:24:12 +0100

    feat: parsing data from args in cmd

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let (query, filename) = parse_config(&args);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
 }
