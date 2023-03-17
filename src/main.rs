5c6f3cbd87a67f88d4999557376a2c2238d58c62
xpanvictor <xpanvictor@gmail.com>
2023-03-17 20:12:37 +0100

    feat: reads file and refrs config to a struct to provide context to code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
+
+    println!("Searching for {} in {}", config.query, config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading file");
+
+    println!("With text:\n{}", contents)
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config { query, filename }
 }
