f05674b3ba00d2c35c853c3dbe0245e4a86500b4
xpanvictor <xpanvictor@gmail.com>
2023-03-18 10:16:36 +0100

    fix: uses eprintln macro to print error to standard error rather than std output.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|error| {
-        println!("Problems parsing arguments: \n{}", error);
+        eprintln!("Problems parsing arguments: \n{}", error);
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
